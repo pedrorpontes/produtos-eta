e4ca4e42594c6b963d9c38d89386ea42310f1b4a
Pedro Pontes <pedrorpontes@gmail.com>
2024-11-28 21:09:29 -0300

    project created

---
+++ src/main/java/br/com/eta/produtos_eta/ProdutosEtaApplication.java
@@ -0,0 +1,13 @@
+package br.com.eta.produtos_eta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosEtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosEtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eta/produtos_eta/ProdutosEtaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eta.produtos_eta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosEtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
